56a27f73f8018c36a38fa247e2d5845bd0590a9c
Ashish Sarkar <ashishsarkar2000@gmail.com>
2019-12-11 12:51:11 +0530

    updating script
    Updating script with CW agent, Creating directories for executing the further log scripts

--- cw_agent_install.sh
+++ cw_agent_install.sh
@@ -27,3 +27,40 @@ sudo echo "
 
 sudo chmod 444 /opt/aws/amazon-cloudwatch-agent/bin/config.json 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
+
+
+
+# Create directories
+mkdir /home/ec2-user/MCOPD_866_sprint
+cd /home/ec2-user/MCOPD_866_sprint && mkdir bash_script_error_tail error_log_dir new_log_file_dir python_script
+
+
+# create bash_script_error_tail and input file to it
+sudo echo "
+tail -1 /home/ec2-user/MCOPD_866_sprint/error_log_dir/error.log > /home/ec2-user/MCOPD_866_sprint/new_log_file_dir/new_log_file.log
+python cw_scripts.py
+" >> /home/ec2-user/MCOPD_866_sprint/bash_script_error_tail/error_tail.sh
+chmod 777 /home/ec2-user/MCOPD_866_sprint/bash_script_error_tail/error_tail.sh
+
+
+# create error_log_dir and input file to it
+sudo echo "
+1.1.4.184 - - [10/Nov/2019:10:22:06 +0000] "GET / HTTP/1.1" 200 3700 "-" "ELB-HealthChecker/2.0" "-"
+" >> /home/ec2-user/MCOPD_866_sprint/error_log_dir/error.log
+
+
+# create new_log_file_dir and input file to it
+sudo echo "
+1.1.4.184 - - [10/Nov/2019:10:22:06 +0000] "GET / HTTP/1.1" 200 3700 "-" "ELB-HealthChecker/2.0" "-"
+" >> /home/ec2-user/MCOPD_866_sprint/new_log_file_dir/new_log_file.log
+
+
+# create error_log_dir and input file to it
+sudo echo "
+1.1.4.184 - - [10/Nov/2019:10:22:06 +0000] "GET / HTTP/1.1" 200 3700 "-" "ELB-HealthChecker/2.0" "-"
+" >> /home/ec2-user/MCOPD_866_sprint/python_script/cw_scripts.py
+chmod 777 /home/ec2-user/MCOPD_866_sprint/python_script/cw_scripts.py
+
+sudo echo "
+* * * * * /home/ec2-user/MCOPD_866_sprint/bash_script_error_tail/error_tail.sh
+" >> crontab
