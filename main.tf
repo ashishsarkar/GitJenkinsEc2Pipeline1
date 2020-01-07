60361a535573e136fca1bc1232f1b96a581ef1ed
Ashish Sarkar <ashishsarkar2000@gmail.com>
2020-01-07 09:15:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,18 +3,18 @@ resource "aws_cloudwatch_log_group" "cw_log_group" {
 }
 
 
-resource "aws_instance" "cw_agent_instance" {
-	ami = "${var.ami_id_name}"
-	instance_type = "${var.instance_type_name}"
-	key_name = "${var.key_pair_name}"
-	user_data = "${file("cw_agent_install.sh")}"
-    iam_instance_profile = "${var.iam_instance_profile}"
-	tags = {
-    Name  = "${var.name_tag}"
-    Email = "${var.owner_tag}"
-    Owner = "${var.email_tag}"
-  }
-}
+# resource "aws_instance" "cw_agent_instance" {
+# 	ami = "${var.ami_id_name}"
+# 	instance_type = "${var.instance_type_name}"
+# 	key_name = "${var.key_pair_name}"
+# 	user_data = "${file("cw_agent_install.sh")}"
+#     iam_instance_profile = "${var.iam_instance_profile}"
+# 	tags = {
+#     Name  = "${var.name_tag}"
+#     Email = "${var.owner_tag}"
+#     Owner = "${var.email_tag}"
+#   }
+# }
 
 
 resource "aws_cloudwatch_log_stream" "cw_log_group_stream" {
